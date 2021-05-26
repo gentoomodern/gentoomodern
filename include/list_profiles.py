98ec3641517b80481ac3f5f63002fd810327ceab
Colin Gilbert <colingilbert86@gmail.com>
2021-05-26 08:50:32 +0000

    Add numbering for lists

--- include/list_profiles.py
+++ include/list_profiles.py
@@ -6,8 +6,12 @@ from include.gentoomuch_common import arch_config_path, current_profile_path, pr
 
 def list_profiles():
   # Get currently active profile
+  if not os.path.isfile(current_profile_path):
+    exit('No profile path config file found at ' + current_profile_path)
   current = open(current_profile_path).read().strip()
   print("Listing compatible system profiles:")
+  ctr = 1
   for p in profiles_amd64:
-    print(('[*] ' if p == current else '[ ] ') + p)
+    print(('[*] ' if p == current else '[ ] ') + str(ctr) + ' ' + p)
+    ctr += 1
   exit()
--- include/list_stages.py
+++ include/list_stages.py
@@ -4,11 +4,15 @@ import os, re
 from include.gentoomuch_common import stage_defines_path, current_stage_path
 
 def list_stages():
- # Get currently active stage
+  # Get currently active stage
+  if not os.path.isfile(current_stage_path):
+    exit('No stage path config file found at ' + current_stage_path)
   current = open(current_stage_path).read().strip()
   print("Listing user-defined stages:")
+  ctr = 1
   for dirpath, dirs, files in os.walk(stage_defines_path):
     if not dirs:
       d = re.sub(stage_defines_path, '', dirpath)
-      print(('[*] ' if d == current  else '[ ] ') + d)
+      print(('[*] ' if d == current else '[ ] ') + str(ctr) + ' ' + d)
+      ctr += 1
   exit()
